0402b2a05f296a750a409204f9e7495533a6145f
bhagwat-chate <bhagwat.chate25@gmail.com>
2021-10-02 16:48:00 +0530

    data save functionality added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -7,3 +7,7 @@ def read_yaml(path_to_yaml: str) -> dict:
 
     return content
 
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path)
+        print(f'directory is created at {dir_path}')
